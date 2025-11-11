dbdd523e731391b09089f946e4aebae9f478560b
a02-trial <cipin23@gmail.com>
2025-11-12 06:59:07 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export ARCH=arm
 export CROSS_COMPILE=$(pwd)/toolchain/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9-lineage-19.1/bin/arm-linux-androideabi-
 export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
